e9371ed13239358e1ea70e043b72b6775088abd1
douglasRondini <douglasfeliperondine@gmail.com>
2025-01-26 14:16:19 -0400

    criei meu projetinho do desafio, adicionando os conteudos educacionais e um vararg de alunos que serão matriculados no curso.

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,40 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+data class ConteudoEducacional(val nome: String, val duraçao: Int)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+class Aluno(val nome: String) {
+    override fun toString(): String {
+        return nome
+    }
+}
 
-class Usuario
-
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
-    val inscritos = mutableListOf<Usuario>()
+data class Formacao(val nome: String, var nivel: String, val conteudos: List<ConteudoEducacional>) {
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    private val alunosMatriculados = mutableListOf<Aluno>()
+    private val formaçaokotlin: String = "Formação Kotlin"
+    
+    fun matricular(vararg alunos: Aluno) {
+        alunosMatriculados.addAll(alunos)
+     
+    }
+    
+    fun mostrarAlunosMatriculados() {
+        if (alunosMatriculados.isEmpty()) {
+            println("Não há alunos matriculados na formação $formaçaokotlin")
+        } else {
+            println("Os alunos $alunosMatriculados foram matriculados com sucesso no curso $formaçaokotlin, eles já podem dar o seu primeiro passo para sua formação")
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val kotlinBasico = ConteudoEducacional("Kotlin Básico", 10)
+    val kotlinAvancado = ConteudoEducacional("Kotlin Avançado", 20)
+    
+    val formacaoKotlin = Formacao("Formação Kotlin", "Intermediário", listOf(kotlinBasico, kotlinAvancado))
+    
+    val aluno1 = Aluno("Alice")
+    val aluno2 = Aluno("Bob")
+    
+    formacaoKotlin.matricular(aluno1, aluno2)
+    formacaoKotlin.mostrarAlunosMatriculados()
 }
+    
